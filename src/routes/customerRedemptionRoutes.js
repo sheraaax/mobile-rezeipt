af9b428e5b459045025c0308c3923630f2565ff1
Sharifah Asilah <asilah2110@gmail.com>
2021-01-05 04:02:59 +0800

    decrement discountUnit after customer redeem reward

--- src/routes/customerRedemptionRoutes.js
+++ src/routes/customerRedemptionRoutes.js
@@ -48,15 +48,13 @@ router.post('/customerRedemption', async(req, res) => {
             console.log(find);
         
         if(!find){ 
+                Redemption.decrement({discountUnit: 1}, { where: {id: redemptionId} })
                 const customer_redemption = new CustomerRedemption({ customerId, redemptionId});
                 await customer_redemption.save();
                 return res.status(200).send({ customer_redemption });
         }
-        
         return res.status(422).send({ error: 'You have already redeemed this reward!'});
-    
-        }
-        
+    }      
 );
 
 router.get('/customerRedemption/:id', async (req, res) => {
